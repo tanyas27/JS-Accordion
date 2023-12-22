994774571345dbd385e1cee94caa9d76906111d8
Tanya Singh <52325969+tanyas27@users.noreply.github.com>
2023-12-22 18:41:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ accordionContainer.forEach((item, index) => {
   let label = item.querySelector('.label');
 
   label.addEventListener('click', () => {
-    let description = item.querySelector('.content');
-    description.classList.toggle('hidden');
-    let icon = item.querySelector('img');
-    if (description.classList.contains('hidden')) {
-      // Scrollheight property return the height of
-      // an element including padding
-      //description.style.height = `${description.scrollHeight}px`;
-      icon.src = './assets/images/icon-plus.svg';
-    } else {
-      icon.src = './assets/images/icon-minus.svg';
+    openContent(item, index);
+  });
+
+  label.addEventListener('keyup', (event) => {
+    if (event.code === 'Enter') {
+      openContent(item, index);
     }
-    // function to pass the index number of clicked label
-    removeOpenedContent(index);
   });
 });
+
+function openContent(item, index) {
+  let description = item.querySelector('.content');
+  description.classList.toggle('hidden');
+  let icon = item.querySelector('img');
+  if (description.classList.contains('hidden')) {
+    // Scrollheight property return the height of
+    // an element including padding
+    //description.style.height = `${description.scrollHeight}px`;
+    icon.src = './assets/images/icon-plus.svg';
+  } else {
+    icon.src = './assets/images/icon-minus.svg';
+  }
+  // function to pass the index number of clicked label
+  removeOpenedContent(index);
+}
 
 function removeOpenedContent(index) {
   accordionContainer.forEach((item2, index2) => {
